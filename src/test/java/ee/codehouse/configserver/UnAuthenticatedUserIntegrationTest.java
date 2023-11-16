8ab8e61cc51a4cfedfb0cfe770b17055eae69fff
markus.karileet <markus@helmes.com>
2023-11-16 12:38:13 +0200

    * add integration tests * fix security config * update actions, documentation

---
+++ src/test/java/ee/codehouse/configserver/UnAuthenticatedUserIntegrationTest.java
@@ -0,0 +1,24 @@
+package ee.codehouse.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest(classes = ConfigServerApplication.class)
+@AutoConfigureMockMvc
+class UnAuthenticatedUserIntegrationTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+	@Test
+	void getApplicationConfig() throws Exception {
+		mockMvc.perform(get("/application/dev"))
+				.andExpect(status().isUnauthorized());
+	}
+
+}
